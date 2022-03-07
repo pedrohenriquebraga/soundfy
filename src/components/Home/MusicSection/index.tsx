8c5faf75b4624c239439d5d88081c7627ca13f63
pedrohenriquebraga <gamesantos7@gmail.com>
2022-03-07 19:18:31 -0300

    remove animation

--- src/components/Home/MusicSection/index.tsx
+++ src/components/Home/MusicSection/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import MusicCard from "../MusicCard";
 import { IMusicData } from "../../../@types/interfaces";
 import { MusicsContainer } from "./styles";
-import { MotiView } from "moti";
 
 export interface IMusicSectionProps {
   content: IMusicData[];
@@ -12,24 +11,7 @@ const MusicSection: React.FC<IMusicSectionProps> = ({ content }) => {
   return (
     <MusicsContainer>
       {content.map((music, index) => (
-        <MotiView
-          key={index}
-          from={{
-            opacity: 0,
-            translateX: -100,
-          }}
-          animate={{
-            opacity: 1,
-            translateX: 0,
-          }}
-          delay={50 + index * 50}
-          transition={{
-            type: "timing",
-            duration: 500,
-          }}
-        >
-          <MusicCard music={music} />
-        </MotiView>
+        <MusicCard key={index} music={music} />
       ))}
     </MusicsContainer>
   );
